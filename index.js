5cb6a7831dc991617cd6cfe05bc709a7fec7dd07
Curious One <35799161+curious-01@users.noreply.github.com>
2022-12-23 09:33:06 +0000

    use web push

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
-const express = require("express");
+//Express
+const express = require('express');
 
+//cors
+const cors = require('cors');
+
+//web-push
+const webpush = require('web-push');
+
+//body-parser
+const bodyParser = require('body-parser');
+
+//path
+const path = require('path');
+
+//using express 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+//using cors
+app.use(cors({
+    origin: '*'
+}));
 
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+//using bodyparser
+app.use(bodyParser.json())
+
+const port = 5000;
+
+//storing the keys in variables
+const publicVapidKey = 'BNjQXaxU5hCoN77_FW5-shUoeZbh3er7J8lADAcPTjfxAUm4bYEkgabyKluYYNF5Js27GLqS92X6cCgFQr1zDsI';
+const privateVapidKey = 'F6xlYfBO8srilgEtDyGV_tHDsUd8e7B_ENSi3Xq6Gvo';
+
+//setting vapid keys details
+webpush.setVapidDetails('mailto:mercymeave@section.com', publicVapidKey,privateVapidKey);
+
+//subscribe route
+app.get('/', (req, res)=>{
+    res.status(200).json({'Server running on port': port})    
+})
+
+
+//subscribe route
+app.post('/subscribe', (req, res)=>{
+    //get push subscription object from the request
+    const res_sub = JSON.parse(req.body.sub);
+    console.log(res_sub);
+
+    //send status 201 for the request
+    res.status(201).json({})
+
+    //create paylod: specified the detals of the push notification
+    const payload = JSON.stringify({
+        title: "From push-express server",
+        body: "Subscribed!",
+        icon: "https://merchant-beta.web.app/static/media/logo.6404ae5bed5d141294430ce541e126bf.svg",
+      });
+
+    //pass the object into sendNotification fucntion and catch any error
+    webpush.sendNotification(res_sub, payload).catch(err=> console.error(err));
+})
+
+app.listen(port, ()=>{
+    console.log(`server started on ${port}`)
 });
 
 // Export the Express API
